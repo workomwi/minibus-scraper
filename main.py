23becc3896da86aa428996cff2d5f0d89313e1f2
omwi <workomwi@gmail.com>
2023-10-04 22:08:31 +0300

    working version

--- main.py
+++ main.py
@@ -1,170 +1,44 @@
-import requests
-from ratelimit import limits, sleep_and_retry
-import datetime
-import sys
+from datetime import date, time
+from ratelimit import sleep_and_retry, limits
 
-from tripservice import MinskExpressService, AvtoslavaService
+from telegram import send_tg_message
+from tripservice import TripService
+from const import MINSK_EXPRESS_STATION_ID, AVTOSLAVA_STATION_ID
 
-
-minsk_express_service = MinskExpressService('https://mogilevminsk.by')
-avto_slava_service = AvtoslavaService('https://avto-slava.by')
-
+minsk_express_service = TripService('https://mogilevminsk.by', MINSK_EXPRESS_STATION_ID)
+avto_slava_service = TripService('https://avto-slava.by', AVTOSLAVA_STATION_ID)
 
 
-
-
-BASE_URL = 'https://mogilevminsk.by'
-CITY_ID = 2
-STATION_ID = 87
-
-
-booked_trip = {
-  # 'time': None
-  'time': datetime.time(18, 0)
-}
-can_book = True
-
-
-
-
-
-def get_trips(date: datetime.date):
-  RELATIVE_URL = '/timetable/trips'
-  body = {
-    'date': date.strftime('%Y-%m-%d'),
-    'from_city': CITY_ID
-  }
-  response = requests.post(f'{BASE_URL}{RELATIVE_URL}', data=body)
-  if response.status_code != 200:
-    print(f'ERROR. {response.status_code} status. {RELATIVE_URL}, body={body}')
-    return []
-  
-  # result = json.loads(response.text)
-  result = response.json()
-  return result['data']['trips']
-
-
-def book_trip(trip: object, phone=PHONE1):
-  RELATIVE_URL = '/timetable/reservation/'
-  body = {
-    'firstname': 'Владислав',
-    'lastname': '',
-    'middlename': '',
-    'date': trip['date'],
-    'departure_time': trip['departure_time'],
-    'trip_id': trip['id'],
-    'phone': PHONE1,
-    'seats': {'1':1},
-    'weekday': trip['weekday'],
-    'route_id': trip['route_id'],
-    'station': STATION_ID,
-    'station_out': '',
-    'description': '',
-    'needsms': 0,
-    'pay_method': None,
-    'location_url':'https://mogilevminsk.by/'
-  }
-
-  response = requests.post(f'{BASE_URL}{RELATIVE_URL}', json=body)
-  result = response.json()
-  data = result.get('data', None)
-  if data is None:
-    send_tg_message('something fucked up, data is None from book trip')
-    sys.exit()
-
-  success = data.get('every_was_fine', None)
-  if success is None:
-    send_tg_message(f'error: {data}')
-    global can_book
-    if phone == PHONE2:
-      can_book = False
-      send_tg_message('cant book on phone2, end')
-    book_trip(trip, PHONE2)
-    return
-
-  if phone == PHONE2:
-    send_tg_message('cant book; booked on phone2')
-
-  booked_trip['time'] = get_trip_departure_time(trip)
-
-  send_tg_message(f'booked trip(phone: {phone}) on {trip["departure_time"]}')
-
-
-
-  
-
-
-def find_closest_trip(trips: list[dict], target_time: datetime.time):
-  if len(trips) == 0:
-    return None
-  
-  return min(trips, key=lambda t: abs(
-    datetime.datetime.combine(datetime.date.today(), datetime.time(int(t['departure_time'][0:2]),int(t['departure_time'][3:5]))) - datetime.datetime.combine(datetime.date.today(), target_time)))
-  
-def find_closest_time(time1: datetime.time, time2: datetime.time, target_time: datetime.time):
-  today = datetime.date.today()
-  return min([time1, time2], key=lambda t: abs(datetime.datetime.combine(today, t) - datetime.datetime.combine(today, target_time)))
-
-def get_trip_departure_time(trip: dict) -> datetime.time:
-  return datetime.time(
-    int(trip['departure_time'][0:2]),
-    int(trip['departure_time'][3:5])
-  )
-
-# latest trip before time_end and after booked trip/time_start
-def find_suitable_trip(trips: list[dict], time_start: datetime.time, time_end: datetime.time, target_time: datetime.time):
-  suitable_trips = []
-  for trip in trips.values():
-    departure_time = get_trip_departure_time(trip)
-    if departure_time > time_end or departure_time < time_start:
-      continue
-    if trip['free_seats'] < 1:
-      continue
-    if not trip['active']:
-      continue
-    # if booked_trip['datetime'] is not None and booked_trip['datetime'] >= departure_time:
-    #     continue
-
-    suitable_trips.append(trip)
-  closest_suitable = find_closest_trip(suitable_trips, target_time)
-  if booked_trip['time'] is None:
-    return closest_suitable
-
-  closest_time = find_closest_time(get_trip_departure_time(closest_suitable), booked_trip['time'], target_time)
-  if closest_time == booked_trip['time']:
-    return None
-  return closest_suitable
-
-@sleep_and_retry
-@limits(1, 300)
-def lookup_trip(date_of_interest, time_start, time_end, target_time):
-  trips = get_trips(date_of_interest)
-  suitable_trip = find_suitable_trip(trips, time_start, time_end, target_time)
+def check_trips(service: TripService, target_date: date, t_start: time, t_end: time, t_target: time):
+  trips = service.get_trips(target_date)
+  suitable_trip = service.find_suitable(trips, t_start, t_end, t_target)
+  print(f'Suitable: {suitable_trip}')
   if suitable_trip is None:
     return
-  message = f'''Найдена поездка {suitable_trip['date']} {suitable_trip['departure_time']}
-{suitable_trip['price']} р.
-{suitable_trip['free_seats']} свободных мест
-{BASE_URL}'''
-  # send_tg_message(message)
-  # book_trip(suitable_trip)
+  service.book_trip(suitable_trip, 'Владислав')
 
+
+count = 0
+
+@sleep_and_retry
+@limits(1, 60)
+def lookup_trips(target_date: date, t_start: time, t_end: time, t_target: time):
+  global count
+  count += 1
+  if count == 10:
+    count = 0
+    send_tg_message('active')
+  # check_trips(minsk_express_service, target_date, t_start, t_end, t_target)
+  check_trips(avto_slava_service, target_date, t_start, t_end, t_target)
 
 
 def main():
-  date_of_interest = datetime.date(2023, 10, 11)
-  time_start = datetime.time(16, 30)
-  time_end = datetime.time(19, 30)
-  target_time = datetime.time(18, 00)
+  target_date = date(2023, 10, 9)
+  t_start = time(16, 30)
+  t_end = time(19, 30)
+  t_target = time(17, 31)
+  while minsk_express_service.can_book or avto_slava_service.can_book:
+    lookup_trips(target_date, t_start, t_end, t_target)
 
-  # date_of_interest = datetime.date(2023, 10, 10)
-  # time_start = datetime.time(16, 30)
-  # time_end = datetime.time(19, 30)
-  # target_time = datetime.time(17, 31)
 
-  while can_book:
-    lookup_trip(date_of_interest, time_start, time_end, target_time)
-  
-
-if __name__ == '__main__':
-  main()
+main()
--- tripservice.py
+++ tripservice.py
@@ -1,25 +1,70 @@
-import abc
-from abc import ABC, abstractmethod
-import json
-from datetime import date
-from typing import Self
+from datetime import date, datetime, time
 import requests
 
-from Trip import Trip
+from trip import Trip
 from telegram import send_tg_message
-from const import CITY_ID, STATION_ID
+from const import CITY_ID, PHONE1, PHONE2
 
-class TripService(ABC):
-  can_book = True
+class TripService:
+  booked_trip: Trip = None
 
-  def __init__(self, domain_url: str, booked_trip: Trip = None) -> None:
+  def __init__(self, domain_url: str, station_id: int) -> None:
     self.domain_url = domain_url
+    self.can_book = True
+    self.station_id = station_id
 
-  @abstractmethod
+
+  def get_departure_datetime(self, trip: Trip) -> datetime:
+    return datetime.fromisoformat(trip.datetime)
+    
+  def get_departure_time(self, trip: Trip) -> time:
+    return time(
+      int(trip.departure_time[0:2]),
+      int(trip.departure_time[3:5])
+    )
+  
+  def find_closest(self, trips: list[Trip], t_target: time) -> Trip:
+    if len(trips) == 0:
+      return None
+    
+    today = date.today()
+    return min(trips, key=lambda t: abs(datetime.combine(today, self.get_departure_time(t)) - datetime.combine(today, t_target)))
+    
+
+  def find_suitable(self, trips: list[Trip], t_start: time, t_end: time, t_target: time):
+    suitable_trips = []
+    for trip in trips:
+      departure_time = self.get_departure_time(trip)
+      if departure_time > t_end or departure_time < t_start:
+        continue
+      if trip.free_seats < 1:
+        continue
+      if not trip.active:
+        continue
+      # if booked_trip['datetime'] is not None and booked_trip['datetime'] >= departure_time:
+      #     continue
+
+      suitable_trips.append(trip)
+    closest_suitable = self.find_closest(suitable_trips, t_target)
+    if self.booked_trip is None:
+      return closest_suitable
+
+    closest = self.find_closest([self.booked_trip, closest_suitable], t_target)
+    if closest.departure_time == self.booked_trip.departure_time:
+      return None
+    return closest
+
+  
+  def send_log(self, relative_url: str, log: str):
+    message = f'{self.domain_url}{relative_url}\n{log}'
+    print(message)
+    send_tg_message(message)
+
+
   def get_trips(self, target_date: date):
-    relative_url = '/timetable/reservation/'
+    relative_url = '/timetable/trips/'
     body = {
-      'date': date.strftime('%Y-%m-%d'),
+      'date': target_date.strftime('%Y-%m-%d'),
       'from_city': CITY_ID
     }
     response = requests.post(f'{self.domain_url}{relative_url}', data=body)
@@ -29,60 +74,78 @@ class TripService(ABC):
     
     result = response.json()
     trips = result['data']['trips']
-    res = [Trip(**trip_json) for trip_json in trips]
-    return res
-
-
-  @abstractmethod
-  def book_trip(self, trip: Trip, firstname: str, phone: str):
+    trips = [Trip(**trip_json) for trip_json in trips.values()]
+    trips = [trip for trip in trips if self.get_departure_datetime(trip).date() == target_date]
+    return trips
+  
+    
+  def book_trip(self, trip: Trip, firstname: str, phone: str = PHONE1):
     relative_url = '/timetable/reservation/'
     body = {
       'firstname': firstname,
       'lastname': '',
       'middlename': '',
-      'date': trip['date'],
-      'departure_time': trip['departure_time'],
-      'trip_id': trip['id'],
+      'date': trip.date,
+      'departure_time': trip.departure_time,
+      'trip_id': trip.id,
       'phone': phone,
       'seats': {'1':1},
-      'weekday': trip['weekday'],
-      'route_id': trip['route_id'],
-      'station': STATION_ID,
+      'weekday': trip.weekday,
+      'route_id': trip.route_id,
+      'station': self.station_id,
       'station_out': '',
       'description': '',
       'needsms': 0,
       'pay_method': None,
-      'location_url':'https://mogilevminsk.by/'
+      'location_url': f'{self.domain_url}/'
     }
 
-    response = requests.post(f'{domain_url}{relative_url}', json=body)
+    if not self.can_book:
+      message = 'Error: Was not able to order trip. Field can_book is False'
+      self.send_log(relative_url, message)
+      return None
+
+    response = requests.post(f'{self.domain_url}{relative_url}', json=body)
     result = response.json()
     data = result.get('data', None)
+    
     if data is None:
-      send_tg_message('something fucked up, data is None from book trip')
-      sys.exit()
+      message = 'Error: data is None'
+      self.send_log(relative_url, message)
+      return None
+    
+    if 'no_free_seats' in data:
+      message = 'Error: no free seats'
+      self.send_log(message)
+      return None
+
 
     success = data.get('every_was_fine', None)
     if success is None:
-      send_tg_message(f'error: {data}')
-      global can_book
-      if phone == PHONE2:
-        can_book = False
-        send_tg_message('cant book on phone2, end')
-      book_trip(trip, PHONE2)
-      return
+      message = f'Error: {data}'
+      self.send_log(relative_url, message)
+      
+      if phone == PHONE2 and 'trip_max_seat_limit' in data:
+        self.can_book = False
+        message = 'Error: Hit trip limit on 2nd phone. Service disabled'
+        self.send_log(relative_url, message)
+        return None
+      
+      if 'trip_max_seat_limit' in data:
+        return self.book_trip(trip, firstname, PHONE2)
+      
+      return None
+
+    success_message = f'''Ordered trip on {phone}
+{trip.date} {trip.departure_time}
+{trip.route}
+{trip.transport}'''
+    self.send_log(relative_url, success_message)
+    self.booked_trip = trip
 
     if phone == PHONE2:
-      send_tg_message('cant book; booked on phone2')
+      self.can_book = False
+      message = 'Ordered both. Service disabled'
+      self.send_log(relative_url, message)
 
-    booked_trip['time'] = get_trip_departure_time(trip)
-
-    send_tg_message(f'booked trip(phone: {phone}) on {trip["departure_time"]}')
-
-
-class MinskExpressService(TripService):
-  pass
-
-
-class AvtoslavaService(TripService):
-  pass
+    return trip
